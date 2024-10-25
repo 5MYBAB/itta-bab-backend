8c9104ee59f4d7531792bd62e78f0f0dca3b2437
enking <engur9902@gmail.com>
2024-10-25 12:51:47 +0900

    feat: 게시글 작성 관련 기능 개발

--- Monolithic/src/main/java/com/fivemybab/ittabab/board/command/application/controller/PostCommandController.java
+++ Monolithic/src/main/java/com/fivemybab/ittabab/board/command/application/controller/PostCommandController.java
@@ -3,12 +3,14 @@ package com.fivemybab.ittabab.board.command.application.controller;
 import com.fivemybab.ittabab.board.command.application.dto.CreatePostDto;
 import com.fivemybab.ittabab.board.command.application.dto.UpdatedPostDto;
 import com.fivemybab.ittabab.board.command.application.service.PostCommandService;
+import com.fivemybab.ittabab.security.util.CustomUserDetails;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import java.time.LocalDateTime;
 
@@ -20,25 +22,28 @@ public class PostCommandController {
 
     private final PostCommandService postCommandService;
 
-
-    //게시판 작성(create)
+    // 게시판 작성 (create)
     @PostMapping
     @Operation(summary = "게시판 글 작성", description = "게시판에 글을 작성(등록)합니다.")
-    public ResponseEntity<CreatePostDto> createPost(@RequestBody CreatePostDto createPostDto, @RequestParam Long userId) {
+    public ResponseEntity<CreatePostDto> createPost(
+            @RequestBody CreatePostDto createPostDto,
+            @AuthenticationPrincipal CustomUserDetails loginUser) {
+
         createPostDto.setCreateDate(LocalDateTime.now());
-        CreatePostDto createdPost = postCommandService.createPost(createPostDto, userId);
+        createPostDto.setUserId(loginUser.getUserId()); // 로그인된 사용자의 userId 설정
+        CreatePostDto createdPost = postCommandService.createPost(createPostDto, loginUser.getUserId());
         return new ResponseEntity<>(createdPost, HttpStatus.CREATED);
     }
 
-    //게시판 삭제(delete)
+    // 게시판 삭제 (delete)
     @DeleteMapping("/{postId}")
     @Operation(summary = "게시판 글 삭제", description = "게시판의 글을 삭제합니다.")
-    public ResponseEntity<Void> deletePost(@PathVariable final Long postId){
+    public ResponseEntity<Void> deletePost(@PathVariable final Long postId) {
         postCommandService.deletePost(postId);
         return ResponseEntity.noContent().build();
     }
 
-    //게시판 수정 (update)
+    // 게시판 수정 (update)
     @PutMapping("/{postId}")
     @Operation(summary = "게시판 글 수정", description = "게시판의 글을 수정합니다.")
     public ResponseEntity<Void> updatePost(
