7b01fea0b1227248ac1a46eb7407495a8a203a81
seolbin01 <se01bin@suwon.ac.kr>
2024-10-26 03:54:11 +0900

    feat: 신고 관련 조회 api 추가

--- Monolithic/src/main/java/com/fivemybab/ittabab/report/query/controller/ReportQueryController.java
+++ Monolithic/src/main/java/com/fivemybab/ittabab/report/query/controller/ReportQueryController.java
@@ -2,6 +2,7 @@ package com.fivemybab.ittabab.report.query.controller;
 
 import com.fivemybab.ittabab.report.query.dto.ReportDto;
 import com.fivemybab.ittabab.report.query.service.ReportQueryService;
+import com.fivemybab.ittabab.security.util.CustomUserDetails;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
@@ -10,6 +11,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -28,9 +30,20 @@ public class ReportQueryController {
     // 모든 신고 조회
     @Operation(summary = "모든 신고 조회(관리자)")
     @GetMapping("/admin")
-    public ResponseEntity<List<ReportDto>> getAllReports() throws NotFoundException {
+    public ResponseEntity<List<ReportDto>> getAllReports(){
 
         List<ReportDto> reportList =reportQueryService.findReportList();
+        return new ResponseEntity<>(reportList, HttpStatus.OK);
+    }
+
+    // 특정 유저가 한 신고 조회
+    @Operation(summary = "특정 유저가 한 신고 조회")
+    @GetMapping("/user")
+    public ResponseEntity<List<ReportDto>> getAllMyReports(@AuthenticationPrincipal CustomUserDetails loginUser){
+
+        Long userId = loginUser.getUserId();
+
+        List<ReportDto> reportList =reportQueryService.findMyReportList(userId);
         return new ResponseEntity<>(reportList, HttpStatus.OK);
     }
 
--- Monolithic/src/main/java/com/fivemybab/ittabab/report/query/mapper/ReportMapper.java
+++ Monolithic/src/main/java/com/fivemybab/ittabab/report/query/mapper/ReportMapper.java
@@ -8,4 +8,6 @@ import java.util.List;
 @Mapper
 public interface ReportMapper {
     List<ReportDto> adminReportList();
+
+    List<ReportDto> userReportList(Long userId);
 }
